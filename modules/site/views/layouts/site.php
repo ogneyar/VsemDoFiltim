fc47caeb8c4734455a7a58370cdac9170dc12cec
ogneyar <ogneyar-ne@ya.ru>
2022-03-27 12:49:11 +0300

    eee

--- modules/site/views/layouts/site.php
+++ modules/site/views/layouts/site.php
@@ -447,11 +447,11 @@ $this->registerJs($script, $this::POS_END);
 
         
         <!--LiveInternet counter--><a href="https://www.liveinternet.ru/click"
-        target="_blank"><img id="licntC6F2" width="88" height="120" style="border:0;padding:10px;" 
+        target="_blank"><img id="licntAAEC" width="88" height="120" style="border:0" 
         title="LiveInternet: показано количество просмотров и посетителей"
         src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7"
-        alt=""/></a><script>(function(d,s){d.getElementById("licntC6F2").src=
-        "https://counter.yadro.ru/hit?t29.6;r"+escape(d.referrer)+
+        alt=""/></a><script>(function(d,s){d.getElementById("licntAAEC").src=
+        "https://counter.yadro.ru/hit?t28.6;r"+escape(d.referrer)+
         ((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+
         (s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
         ";h"+escape(d.title.substring(0,150))+";"+Math.random()})
